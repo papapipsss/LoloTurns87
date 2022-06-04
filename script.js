88cc001b188df310dbac4bbbf0234bc014982d40
Joshua <jlfelipe1@up.edu.ph>
2022-06-04 15:59:52 +0800

    Removed all UST pictures and replaced with temporary photos

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+var temp_photo = "https://www.pixelstalk.net/wp-content/uploads/images5/Soft-Aesthetic-Wallpaper-HD-Free-download.jpg"
+
+
 for(let x = 1; x < 10; x++){
-    const element = '<div class="boxesT"><img src="assets/' + x + '.jpg" alt="" class="image"></div>'
+    const element = '<div class="boxesT"><img src="' + temp_photo + '" alt="" class="image"></div>'
     document.getElementById("sections").innerHTML += element;
 }
 
 
 for (let x = 1; x < 10; x++){
-    const element = '<div class="carousel-item"><img class="carousel-images" src="./assets/' + x + '.jpg" alt="' + x + ' slide"></div>';
+    const element = '<div class="carousel-item"><img class="carousel-images" src="' + temp_photo + '" alt="' + x + ' slide"></div>';
     document.querySelector('.carousel-inner').innerHTML += element;
 }
