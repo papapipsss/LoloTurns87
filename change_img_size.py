9802f1ebc65d76ecd8872fc75221cae03ed8bde0
Joshua <jlfelipe1@up.edu.ph>
2022-06-12 20:06:16 +0800

    Added new photos, videos and vertical carousel

--- change_img_size.py
+++ change_img_size.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 size = 1280, 720
 
-mypath = "C:/Users/Joshua Felipe/OneDrive/Desktop/LOLO/2 Landscape Carousel/"
+mypath = "C:/Users/Joshua Felipe/OneDrive/Desktop/LOLO/3 Portrait/"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 counter = 1
@@ -14,7 +14,7 @@ for i in onlyfiles:
     try:
         im = Image.open(path)
         im.thumbnail(size, Image.ANTIALIAS)
-        im.save("C:/Users/Joshua Felipe/OneDrive/Desktop/LOLO/new-carousel/" + outfile + ".jpg")
+        im.save("C:/Users/Joshua Felipe/OneDrive/Desktop/LOLO/new-vertical/" + outfile + ".jpg")
         counter += 1
     except IOError:
         print("cannot create thumbnail for " + i)
--- script.js
+++ script.js
@@ -1,16 +1,59 @@
-var temp_photo = "https://www.pixelstalk.net/wp-content/uploads/images5/Soft-Aesthetic-Wallpaper-HD-Free-download.jpg"
+var vid_links_relatives = ["13O1hb8crN573GrM6iJ0t0kVfj87h42ZU", "12-dAQvleg0nYCbLIvYWW3RINy69CGZ83", "108QGz-YDJu0NMuIptenD5z_6pbc2bRCC"];
+
+var vid_links_family = [];
+
+var vid_links_acquaintances = []
 
 
+// PHOTO BLOCKS
 for(let x = 1; x < 18; x++){
     const element = '<div class="boxesT"><img src="./assets/new-blocks/' + x + '-edited.jpg" alt="" class="image"></div>'
-    document.getElementById("sections").innerHTML += element;
+    document.getElementById("photo-sections").innerHTML += element;
 }
 
-for (let x = 2; x < 18; x++){
+
+// HORIZONTAL CAROUSEL
+for (let x = 2; x < 27; x++){
     const element = '<div class="carousel-item"><img class="d-block w-100" src="./assets/new-carousel/' + x + '-edited.jpg" alt="' + x + ' slide"></div>';
-    document.querySelector('.carousel-inner').innerHTML += element;
+    document.querySelector('.horizontal').innerHTML += element;
 }
 
+
+// VERTICAL CAROUSEL
+for (let x = 2; x < 33; x++){
+    const element = '<div class="carousel-item"><img class="d-block w-100" src="./assets/new-vertical/0' + x + '-edited.jpg" alt="' + x + ' slide"></div>';
+    document.querySelector('.vertical').innerHTML += element;
+}
+
+
+// FAMILY
+for (let x = 0; x < vid_links_family.length; x++){
+    const element = '<iframe src="https://drive.google.com/file/d/' + vid_links_family[x] + '/preview" width="640" height="480" allow="autoplay" allowfullscreen class="videos"></iframe>'
+    document.getElementById("vid-sections-family").innerHTML += element;
+}
+
+
+// RELATIVES 
+for (let x = 0; x < vid_links_relatives.length; x++){
+    const element = '<iframe src="https://drive.google.com/file/d/' + vid_links_relatives[x] + '/preview" width="640" height="480" allow="autoplay" allowfullscreen class="videos"></iframe>'
+    document.getElementById("vid-sections-relatives").innerHTML += element;
+}
+
+// ACQUAINTANCES 
+for (let x = 0; x < vid_links_acquaintances.length; x++){
+    const element = '<iframe src="https://drive.google.com/file/d/' + vid_links_acquaintances[x] + '/preview" width="640" height="480" allow="autoplay" allowfullscreen class="videos"></iframe>'
+    document.getElementById("vid-sections-acquaintances").innerHTML += element;
+}
+
+// function myFunction() {
+//     var x = document.getElementById("carouselExampleIndicators1");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
+// var temp_photo = "https://www.pixelstalk.net/wp-content/uploads/images5/Soft-Aesthetic-Wallpaper-HD-Free-download.jpg"
 // for(let x = 1; x < 10; x++){
 //     const element = '<div class="boxesT"><img src="' + temp_photo + '" alt="" class="image"></div>'
 //     document.getElementById("sections").innerHTML += element;
